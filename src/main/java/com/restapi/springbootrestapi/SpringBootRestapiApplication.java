f80991b2fef3baf5b30e23cb8e8235c864b14dbc
Jonathan Persaud <JonathanPersaud@Jonathans-MacBook-Pro-39.local>
2018-07-24 22:34:35 -0400

    get requests handle with name param

---
+++ src/main/java/com/restapi/springbootrestapi/SpringBootRestapiApplication.java
@@ -0,0 +1,12 @@
+package com.restapi.springbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestapiApplication.class, args);
+    }
+}
